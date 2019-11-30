2806dbba050b2ca4b1c1470d62a99cf8e89c61f7
popi300 <33000879+popi300@users.noreply.github.com>
2019-11-30 15:25:35 +0100

    Add files via upload

--- japonVisual.py
+++ japonVisual.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 from tkinter import *
 from tkinter.ttk import *
 from networkx import *
@@ -37,15 +41,55 @@ diccNodos = {"Shinagawa": {'f': -1, 'g': -1, 'puntero': -1, 'posH': 0} ,"Osaki":
 
 #Grafo ()
 G = Graph()
-'''listaNodos=["Shinagawa","Osaki","Gotanda","Meguro","Ebisu","Shibuya","Harajuku","Yoyogi","Sobu Yoyogi",
-                            "Yamanote Yoyogi", "Shinjuku","Sobu Shinjuku", "Chuo Shinjuku", "Yamanote Shinjuku","Shin-Okubo",
-                            "Takadanobaba","Mejiro","Ikebukuro","Otsuka","Sugamo",
-                            "Komagome","Tabata","Nishi-Nippori","Nippori","Uguisudani","Ueno","Okachimachi",
-                            "Akihabara", "Yamanote Akihabara", "Sobu Akihabara","Kanda","Tokyo", "Yamanote Tokyo","Chuo Tokyo" ,
-                            "Yurakucho","Shimbashi","Hamamatsucho","Tamachi",
-                            "Ochanomizu", "Sobu Ochanomizu", "Chuo Ochanomizu","Suidobashi","Iidabashi",
-                            "Ichigaya","Yotsuya","Shinanomachi","Sendagaya"]
-G.add_nodes_from(listaNodos)'''
+posNodos={
+"Shinagawa":(327,18),
+"Osaki":(260,9),
+"Gotanda":(215,9),
+"Meguro":(175,9),
+"Ebisu":(120,36),
+"Shibuya":(120,97),
+"Harajuku":(120,150),
+"Yoyogi":(120,230),
+"Sobu Yoyogi":(70,230),
+"Yamanote Yoyogi":(120,230),
+"Shinjuku":(120,273),
+"Sobu Shinjuku":(70,273),
+"Chuo Shinjuku":(92,315),
+"Yamanote Shinjuku":(120,273),
+"Shin-Okubo":(120,360),
+"Takadanobaba":(125,406),
+"Mejiro":(166,467),
+"Ikebukuro":(233,490),
+"Otsuka":(311,488),
+"Sugamo":(350,488),
+"Komagome":(387,488),
+"Tabata":(436,470),
+"Nishi-Nippori":(458,446),
+"Nippori":(465,410),
+"Uguisudani":(465,380),
+"Ueno":(465,350),
+"Okachimachi":(465,317),
+"Akihabara":(465,286),
+"Yamanote Akihabara":(465,286),
+"Sobu Akihabara":(410,286),
+"Kanda":(465,238),
+"Tokyo":(465,192),
+"Yamanote Tokyo":(465,192),
+"Chuo Tokyo":(415,192),
+"Yurakucho":(460,150),
+"Shimbashi":(433,123),
+"Hamamatsucho":(400,91),
+"Tamachi":(365,56),
+"Ochanomizu":(335,309),
+"Sobu Ochanomizu":(314,286),
+"Chuo Ochanomizu":(335,315),
+"Suidobashi":(283,264),
+"Iidabashi":(263,244),
+"Ichigaya":(243,224),
+"Yotsuya":(223,204),
+"Shinanomachi":(195,180),
+"Sendagaya":(146,180)
+}
 
 G.add_edges_from([("Yamanote Shinjuku","Shin-Okubo", {'color':'green', 'weight':1.3}),
                  ("Shin-Okubo","Takadanobaba", {'color':'green', 'weight':1.4}),
@@ -64,7 +108,7 @@ G.add_edges_from([("Yamanote Shinjuku","Shin-Okubo", {'color':'green', 'weight':
                  ("Yamanote Akihabara", "Akihabara", {'color':'grey', 'weight':0.0}),
                  ("Yamanote Akihabara", "Kanda", {'color':'green', 'weight':0.7}),
                  ("Kanda", "Yamanote Tokyo", {'color':'green', 'weight':1.3}),
-                 ("Yamanote Tokyo", "Tokyo", {'color':'grey', 'weight':0.0}),
+                 ("Yamanote Tokyo", "Tokyo", {'color':'blue', 'weight':0.0}),
                  ("Yamanote Tokyo", "Yurakucho", {'color':'green', 'weight':0.8}),
                  ("Yurakucho", "Shimbashi", {'color':'green', 'weight':1.1}),
                  ("Shimbashi", "Hamamatsucho", {'color':'green', 'weight':1.2}),
@@ -77,15 +121,15 @@ G.add_edges_from([("Yamanote Shinjuku","Shin-Okubo", {'color':'green', 'weight':
                  ("Ebisu", "Shibuya", {'color':'green', 'weight':1.6}),
                  ("Shibuya", "Harajuku", {'color':'green', 'weight':1.2}),
                  ("Harajuku", "Yamanote Yoyogi", {'color':'green', 'weight':1.5}),
-                 ("Yamanote Yoyogi", "Yoyogi", {'color':'grey', 'weight':0.0}),
+                 ("Yamanote Yoyogi", "Yoyogi", {'color':'blue', 'weight':0.0}),
                  ("Yamanote Yoyogi", "Yamanote Shinjuku", {'color':'green', 'weight':0.7}),
-                 ("Yamanote Shinjuku", "Shinjuku", {'color':'grey', 'weight':0.0}),
-                 ("Shinjuku", "Chuo Shinjuku", {'color':'grey', 'weight':0.0}),
+                 ("Yamanote Shinjuku", "Shinjuku", {'color':'blue', 'weight':0.0}),
+                 ("Shinjuku", "Chuo Shinjuku", {'color':'blue', 'weight':0.0}),
                  ("Chuo Shinjuku", "Chuo Ochanomizu", {'color':'red', 'weight':7.7}),
-                 ("Chuo Ochanomizu", "Ochanomizu", {'color':'grey', 'weight':0.0}),
+                 ("Chuo Ochanomizu", "Ochanomizu", {'color':'blue', 'weight':0.0}),
                  ("Chuo Ochanomizu", "Chuo Tokyo", {'color':'red', 'weight':2.6}),
-                 ("Chuo Tokyo", "Tokyo", {'color':'grey', 'weight':0.0}),
-                 ("Shinjuku", "Sobu Shinjuku", {'color':'grey', 'weight':0.0}),
+                 ("Chuo Tokyo", "Tokyo", {'color':'blue', 'weight':0.0}),
+                 ("Shinjuku", "Sobu Shinjuku", {'color':'blue', 'weight':0.0}),
                  ("Sobu Shinjuku", "Sobu Yoyogi", {'color':'yellow', 'weight':0.7}),
                  ("Sobu Yoyogi", "Yoyogi", {'color':'grey', 'weight':0.0}),
                  ("Sobu Yoyogi", "Sendagaya", {'color':'yellow', 'weight':1.0}),
@@ -95,16 +139,15 @@ G.add_edges_from([("Yamanote Shinjuku","Shin-Okubo", {'color':'green', 'weight':
                  ("Ichigaya", "Iidabashi", {'color':'yellow', 'weight':1.5}),
                  ("Iidabashi", "Suidobashi", {'color':'yellow', 'weight':0.9}),
                  ("Suidobashi", "Sobu Ochanomizu", {'color':'yellow', 'weight':0.8}),
-                 ("Sobu Ochanomizu", "Ochanomizu", {'color':'grey', 'weight':0.0}),
+                 ("Sobu Ochanomizu", "Ochanomizu", {'color':'blue', 'weight':0.0}),
                  ("Sobu Ochanomizu", "Sobu Akihabara", {'color':'yellow', 'weight':0.9}),
-                 ("Sobu Akihabara", "Akihabara", {'color':'grey', 'weight':0.0}),
+                 ("Sobu Akihabara", "Akihabara", {'color':'blue', 'weight':0.0}),
                  ("Yamanote Yoyogi", "Sobu Yoyogi", {'color' :'grey', 'weight':0.2}),
                  ("Yamanote Shinjuku", "Chuo Shinjuku", {'color':'grey', 'weight':0.2}),
                  ("Yamanote Akihabara", "Sobu Akihabara", {'color':'grey', 'weight':0.2})
                  ])
 
-#nx.draw(G, pos=nx.spectral_layout(G),with_labels=True, font_weight='bold')
-
+colorLinea={'green': 'Yamanote', 'red': 'Chuo', 'yellow': 'Sobu', 'grey': 'Interchange','blue': 'Simbolic'}
 
 def algoritmoA_Estrella(origenNombre, destinoNombre,G) :
     origen = diccNodos[origenNombre]
@@ -198,29 +241,24 @@ def cogerLinea(fila):
             next(reader)
         return next(reader)
     
-def eliminarNodosAux(origenNombre, destinoNombre, G):
+def eliminarNodosAux(G,origenNombre=None, destinoNombre=None):
     L = ['Shinjuku', 'Akihabara', 'Tokyo', 'Ochanomizu' , 'Yoyogi']
     for nodo in L: 
         if (nodo != origenNombre and nodo != destinoNombre) :
            G.remove_node(nodo)
-
-colorLinea={'green': 'Yamanote', 'red': 'Chuo', 'yellow': 'Sobu', 'grey': 'Interchange'}
-
-
 
 def calcularResumenCamino():
     if(seleccionOrigen.current() !=-1 and seleccionDestino.current()!=-1):
         #Habilita el boton de dibujar
         if str(botonPintarGrafo['state']) == 'disabled':
             botonPintarGrafo['state'] = 'normal'
-
+        global camino
         treeResultado.delete(*treeResultado.get_children())
         origenNombre = seleccionOrigen.get()
         destinoNombre = seleccionDestino.get()
         Gcopia = G.copy()
-        eliminarNodosAux(origenNombre, destinoNombre, Gcopia)
+        eliminarNodosAux(Gcopia, origenNombre, destinoNombre)
         camino = algoritmoA_Estrella(origenNombre, destinoNombre, Gcopia.copy())
-        print (camino)
         linea = None
         longCamino = len(camino)
         for i in range(longCamino):
@@ -229,15 +267,59 @@ def calcularResumenCamino():
             elif i==longCamino-1:
                 treeResultado.insert('','end',text=camino[i])
             else:
-                print (linea)
                 if linea==None or colorLinea[Gcopia[camino[i-1]][camino[i]]['color']]!=linea:
-                    linea=colorLinea[Gcopia[camino[i-1]][camino[i]]['color']]
-                    if i != 1:
+                    if colorLinea[Gcopia[camino[i-1]][camino[i]]['color']] == 'Interchange':
+                        linea=colorLinea[Gcopia[camino[i]][camino[i+1]]['color']]
+                    else:
+                         linea=colorLinea[Gcopia[camino[i-1]][camino[i]]['color']]
+                    if i ==1:
+                        treeResultado.insert(padre,'end',text=camino[i])
+                    else:
                         padre = treeResultado.insert('','end',text=camino[i])
+                    treeResultado.set(padre,'linea',linea)
+                    #treeResultado.item(padre,tags=('Yamanote'))
                 else: 
                     treeResultado.insert(padre,'end',text=camino[i])
-                    
 
+def pintarGraph():
+    global camino
+    #top = Toplevel()
+    #top.title('Ruta en el Mapa')
+    #topFrame= Frame(top)
+    #topFrame.pack()
+    #topFrame.config(width=600,height = 600)
+    f = plt.figure(figsize=(18,15),dpi=100,num='Ruta en el Mapa')
+    thismanager = plt.get_current_fig_manager()
+    #Icon made by Freepik from www.flaticon.com
+    thismanager.window.wm_iconbitmap("konoha.ico")
+    a = f.add_subplot(111)
+    plt.axis('off')
+    
+    Gcopia = G.copy()
+    eliminarNodosAux(Gcopia)
+    nodes= Gcopia.nodes()
+    edges = Gcopia.edges()
+    L = ['Shinjuku', 'Akihabara', 'Tokyo', 'Ochanomizu' , 'Yoyogi']
+    caminoNodos = [parada for parada in camino if parada not in L]
+    caminoEdges = [(caminoNodos[i],caminoNodos[i+1]) for i in range(len(caminoNodos)-1)]
+    colorsE = [G[u][v]['color']for u,v in edges]
+    weightsE = [4 + 2.2/(G[u][v]['weight']) for u,v in edges]
+    nx.draw_networkx(Gcopia, posNodos,node_color='grey',node_size=200,width =weightsE,
+                     alpha=0.8, with_labels=True, font_weight='bold', edge_color=colorsE)
+    
+    nx.draw_networkx_edges(Gcopia, posNodos,
+                       edgelist=caminoEdges,
+                       width=[4 + 2.2/Gcopia[caminoNodos[i]][caminoNodos[i+1]]['weight'] for i in range(len(caminoNodos)-1)],
+                       edge_color='b',alpha=0.7)
+
+    xlim=a.get_xlim()
+    ylim=a.get_ylim()
+    plt.show()
+    #canvas = FigureCanvasTkAgg(f,master= frame)
+    #canvas.show()
+    #canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=True)
+    
+    
 root = Tk()
 root.title("Metro Japón")
 root.resizable(0, 0)
@@ -272,18 +354,34 @@ seleccionDestino["values"] = ("Shinagawa","Osaki","Gotanda","Meguro","Ebisu","Sh
                             "Ochanomizu","Suidobashi","Iidabashi","Ichigaya","Yotsuya","Shinanomachi","Sendagaya")
 
 
+camino = None
 
 botonCalcular  = Button (frame, command=calcularResumenCamino, text = "Calcular ruta")
 botonCalcular.grid(row=4, column=0 ,padx=5, pady=5)
 
-botonPintarGrafo = botonPintarGrafo=Button (frame,text = "Dibujar ruta",state=DISABLED)
+botonPintarGrafo=Button (frame,command = pintarGraph,text = "Dibujar ruta",state=DISABLED)
 botonPintarGrafo.grid(row=5, column=0 ,padx=5, pady=5)
 
-labelResultado = Label(frame, text = "Resultado:")
+labelResultado = Label(frame,text = "Resultado:")
 labelResultado.grid(row=0, column=1, sticky=W ,padx=5, pady=5)
 
 treeResultado = Treeview(frame)
+
+
 treeResultado.grid (row=1, column=1, rowspan=5, sticky=W ,padx=5, pady=5)
+treeResultado.config(columns=("linea"))
+treeResultado.column('linea',width=150,anchor='center')
+treeResultado.heading('#0',text='Estación')
+treeResultado.heading('linea',text='Linea')
+
+treeResultado.tag_configure('Chuo', background='red')
+treeResultado.tag_configure('Yamanote', background='green')
+treeResultado.tag_configure('Sobu', background='yellow')
+
+ysb = Scrollbar(frame,orient=VERTICAL, command= treeResultado.yview)
+treeResultado['yscroll'] = ysb.set 
+ysb.grid(row=1, column=2, rowspan=5, sticky='nsew')
+
 
 root.mainloop()
    
